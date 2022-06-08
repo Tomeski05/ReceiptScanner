05a4afcda6dbe39d905a2be49bd6fe8981299cb4
Filip Tomeski <tomeski@protonmail.com>
2022-06-08 20:30:23 +0200

    receiptScanner

--- ReceiptSacanner/ReceiptSacanner/Program.cs
+++ ReceiptSacanner/ReceiptSacanner/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ReceiptSacanner
 {
@@ -6,9 +10,27 @@ namespace ReceiptSacanner
     {
         HttpClient client = new HttpClient();
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            Program program = new Program();
+            await program.GetTodoItems();
         }
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync(
+               "https://interview-task-api.mca.dev/qr-scanner-codes/alpha-qr-gFpwhsQ8fkY1");
+
+            List<Todo> todo = JsonConvert.DeserializeObject<List<Todo>>(response);
+        }
+
+        public class Todo
+        {
+            public string Name { get; set; }
+            public bool Domestic { get; set; }
+            public double Price { get; set; }
+            public int? Weight { get; set; }
+            public string Description { get; set; }
+        }
+
     }
 }
